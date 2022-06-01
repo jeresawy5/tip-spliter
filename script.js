a19526a83286d43b12838f15194151e109b2eaa4
jeresawy5 <jeresawy5@gmail.com>
2022-06-01 17:03:56 -0400

    update

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-
-let subTotal = Document.getElementById('subtotal');
-let tipPer = (Document.getElementById('tipP') * 100);
-let totalPeople = Document.getElementById('numPeople');
-let outPut = Document.getElementById('output');
+let subTotal = 200;//document.getElementById("subTotal");
+let tipPer = 20 / 100;//document.getElementById('tipP') / 100;
+let people = 2;//document.getElementById('totalPeople');
+let outPut //= document.getElementById('output');
 //global variables
 
-Document.getElementById('submit') submit.addEventListener('click', calculate);
-
-
-function withTip = (a, b){
-    return a + b;
-}; 
-// grand total
-
-
-
-function calculate = (c, d){
-    return withTip / totalPeople;
+function calcTip(sub, per){
+    return sub * per;
 };
+
+let tip = calcTip(subTotal, tipPer);
+
+console.log(tip);
+
+function withTip(tip, sub){
+    return tip + sub;
+};
+let total = withTip(tip, subTotal);
+
+console.log(total);
+
+function perPerson(total, people){
+    return total / people;
+};
+
+let totalPerPerson = perPerson(total, people);
+
+console.log(totalPerPerson);
