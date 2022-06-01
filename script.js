6c673f3f90bb81adf5d7fa60bf792230cc6864da
jeresawy5 <jeresawy5@gmail.com>
2022-06-01 15:35:27 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let subTotal = Document.getElementById('subtotal');
+let tipPer = (Document.getElementById('tipP') * 100);
+let totalPeople = Document.getElementById('numPeople');
+let outPut = Document.getElementById('output');
+//global variables
+
+Document.getElementById('submit') submit.addEventListener('click', calculate);
+
+
+function withTip = (a, b){
+    return a + b;
+}; 
+// grand total
+
+
+
+function calculate = (c, d){
+    return withTip / totalPeople;
+};
